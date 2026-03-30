7d31babfca64df518284236b19a7a76c6d833d24
Farhan Nisar <webprocode139@gmail.com>
2026-03-31 01:24:21 +0530

    Api connection

--- src/js/controller.js
+++ src/js/controller.js
@@ -12,3 +12,32 @@ const timeout = function (s) {
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      // 'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886',
+      'https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e898a',
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message}(${res.status}) `);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.sourceUrl,
+      image: recipe.image,
+      servings: recipe.servings,
+      cookingTime: recipe.cookingTime,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
