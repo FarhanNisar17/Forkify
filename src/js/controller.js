1c7c77b4ecc88b7b7ad8ce318061789245a09fd1
Farhan Nisar <webprocode139@gmail.com>
2026-04-05 23:48:44 +0530

    models seperation

--- src/js/controller.js
+++ src/js/controller.js
@@ -35,25 +35,6 @@ const showRecipe = async function () {
     const id = window.location.hash.slice(1);
 
     renderSpinner(recipeContainer);
-    const res = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/${id}`,
-    );
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message}(${res.status}) `);
-
-    let { recipe } = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.sourceUrl,
-      image: recipe.image,
-      servings: recipe.servings,
-      cookingTime: recipe.cookingTime,
-      ingredients: recipe.ingredients,
-    };
-    console.log(recipe);
 
     const markup = `<figure class="recipe__fig">
           <img src="${recipe.image}" alt="Tomato" class="recipe__img" />
---
+++ src/js/model.js
@@ -0,0 +1,23 @@
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  const res = await fetch(`https://forkify-api.jonas.io/api/v2/recipes/${id}`);
+  const data = await res.json();
+
+  if (!res.ok) throw new Error(`${data.message}(${res.status}) `);
+
+  let { recipe } = data.data;
+  state.recipe = {
+    id: recipe.id,
+    title: recipe.title,
+    publisher: recipe.publisher,
+    sourceUrl: recipe.sourceUrl,
+    image: recipe.image,
+    servings: recipe.servings,
+    cookingTime: recipe.cookingTime,
+    ingredients: recipe.ingredients,
+  };
+  console.log(state.recipe);
+};
